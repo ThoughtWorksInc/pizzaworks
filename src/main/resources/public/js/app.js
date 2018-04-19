1da5168c04b3f08c5b9fb2e74a43a4426e350857
Anthony Scatchell <scatchell@gmail.com>
2018-04-19 15:48:21 +0100

    Removing unnecessary edit js logic left over from sample

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,14 +1,14 @@
-$(document)
-    .keyup('input', function (e) {
-        if (e.keyCode === 27) {
-            $(e.target).trigger('resetEscape');
-        }
-    })
-    .click(function (e) {
-        if ($('#todo-edit').length > 0 && !$(e.target).is('#todo-edit')) {
-            $("#edit-form").trigger('submit');
-        }
-    });
+// $(document)
+//     .keyup('input', function (e) {
+//         if (e.keyCode === 27) {
+//             $(e.target).trigger('resetEscape');
+//         }
+//     })
+//     .click(function (e) {
+//         if ($('#pizza-edit').length > 0 && !$(e.target).is('#pizza-edit')) {
+//             $("#edit-form").trigger('submit');
+//         }
+//     });
 
 Intercooler.ready(function () {
     $("[autofocus]:last").focus();
