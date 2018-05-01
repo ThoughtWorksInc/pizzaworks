e8c62413e1cb1a6f272cd198f762ff43aa78d5c5
Anthony Scatchell <scatchell@gmail.com>
2018-05-01 11:40:10 +0100

    Generifying method name to retrieve assigned port

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,14 @@ import static spark.Spark.staticFiles;
 
 public class Main {
     public static void main(String[] args) {
-        port(getHerokuAssignedPort());
+        port(getAssignedPort());
         exception(Exception.class, (e, req, res) -> e.printStackTrace()); // print all exceptions
         staticFiles.location("/public");
 
         PizzaController.initialize();
     }
 
-    static int getHerokuAssignedPort() {
+    static int getAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
