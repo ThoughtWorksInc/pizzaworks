a5a397186b46f90f57786fe50b3f80434e7c764a
Anthony Scatchell <scatchell@gmail.com>
2018-05-10 17:47:54 +0100

    11 [Tom, Anthony] - Copy migration scripts to application server

--- build-scripts/deploy.sh
+++ build-scripts/deploy.sh
@@ -5,6 +5,8 @@ set -e
 echo "---- Copying Jar to Server ----"
 scp  ./jars/*.jar  ec2-user@$PIZZA_WORKS_SERVER:/home/ec2-user/pizzaworks.jar
 scp  build-scripts/start-server.sh  ec2-user@$PIZZA_WORKS_SERVER:/home/ec2-user/
+scp  -r migrations ec2-user@$PIZZA_WORKS_SERVER:/home/ec2-user/
+
 
 echo "---- Starting Server on Remote box ----"
 ssh ec2-user@$PIZZA_WORKS_SERVER -o SendEnv="PGHOST PGPORT PGUSER PGDATABASE PGPW" "chmod +x ./start-server.sh && ./start-server.sh"
