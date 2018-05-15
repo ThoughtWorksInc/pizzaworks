80ffc22a6dfac6e93de2195a62a599570ff00383
Anthony Scatchell <scatchell@gmail.com>
2018-05-15 10:55:07 +0100

    11 [Anthony] - Using go agent config only when on go agent

--- build-scripts/integration-tests.sh
+++ build-scripts/integration-tests.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-source /var/go/.bashrc
-nvm use 5.1.0
+
+if [ "$IS_GO_AGENT" = "true" ]; then
+    echo "Go agent detected - setting npm"
+    source /var/go/.bashrc
+    nvm use 5.1.0
+else
+    echo "Go agent NOT detected - continuing using local environment"
+fi
+
 mvn clean test -Pintegration-tests
