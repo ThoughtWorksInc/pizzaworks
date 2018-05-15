f6c32a1d339d58dac9b1d2a1899a1be3af9b9286
Anthony Scatchell <scatchell@gmail.com>
2018-05-15 12:28:02 +0100

    11 [Anthony] - copying db migration script to server

--- db-scripts/migrate-server.sh
+++ db-scripts/migrate-server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "---- Copying Migrate Script to Server ----"
-scp  ./*.sh  ec2-user@$PIZZA_WORKS_SERVER:/home/ec2-user/
+scp  ./db-scripts/migrate.sh  ec2-user@$PIZZA_WORKS_SERVER:/home/ec2-user/
 scp  -r migrations ec2-user@$PIZZA_WORKS_SERVER:/home/ec2-user/
 
 
