52810497c7c2f6e79bc0def48bd41dcbd6258a69
Bonnie Appleton <bemappleton@gmail.com>
2018-05-16 14:21:08 +0100

    [Bonnie] Uncomment createuser so that init-db scripts will run

--- db-scripts/init-db.sh
+++ db-scripts/init-db.sh
@@ -4,7 +4,7 @@ initdb -U superpizza
 
 bash db-scripts/start_db.sh;
 
-#createuser -s superpizza;
+createuser -s superpizza;
 createdb -U superpizza -O superpizza pizzaworks;
 
 psql -U superpizza pizzaworks -f db-scripts/init-superpizza.sql
