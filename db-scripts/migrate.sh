81cd617dc704faab9cf9a29d5f964f8b4f74a8ee
Arpitha Patil <arpitha.patil@outlook.com>
2018-05-29 13:29:09 +0100

    nvmfix

--- db-scripts/migrate.sh
+++ db-scripts/migrate.sh
@@ -3,15 +3,15 @@
 if [ "$IS_GO_AGENT" = "true" ]; then
     echo "Go agent detected - setting npm"
     source /var/go/.bashrc
-    nvm use 5.1.0
 else
+    source ~/.bashrc
     echo "Go agent NOT detected - continuing using local environment"
 fi
+
+nvm use
 
 if [ -z "$MIGRATE_TO" ]; then
     echo "Migrating back to version: $MIGRATE_TO"
 fi
 
 (cd migrations && pg-migrator postgres://$PGUSER:$PGPW@$PGHOST:$PGPORT/$PGDATABASE $MIGRATE_TO)
-
-
