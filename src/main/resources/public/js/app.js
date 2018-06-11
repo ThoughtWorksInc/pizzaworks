bd4d33965d5c308c4103e5241cc6a87341d79a10
jen <jredfern@thoughtworks.com>
2018-06-11 15:40:35 +0100

    22 [kavi & jen] re-factored filter drop down box

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,53 +1,12 @@
 
-/*-------------- Dropdown functions --------------*/
+document.addEventListener('DOMContentLoaded',function() {
+    document.querySelector('select[name="pizza-options"]').onchange=changeEventHandler;
+},false);
 
-/* When the user clicks on the button, toggle between hiding and showing the dropdown content */
-function renderDropdown() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+function changeEventHandler(event) {
+    // You can use “this” to refer to the selected element.
+    if(event.target.value == 'all-pizzas') new FilterHelper($).showAllPizzas();
+    if(event.target.value == 'veggie-pizzas') new FilterHelper($).showVegetarianPizzas();
+    if(event.target.value == 'vegan-pizzas') new FilterHelper($).showVeganPizzas();
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-};
-
-
-/*-------------- Pizza List functions --------------*/
-
-
-
-function showVegetarianPizzas() {
-    var pizzas = document.getElementById("vegetarian-pizzas");
-    if (pizzas.style.display === 'none') {
-        pizzas.style.display = 'block';
-    }
-    document.getElementById("all-pizzas").style.display = 'none';
-    document.getElementById("vegan-pizzas").style.display = 'none';
-}
-
-function showVeganPizzas() {
-
-
-    // new Hider().hide("vegan");
-    //
-    // new FilterHelper(hider).filterBy("vegan");
-
-    //assert one li exists, and it's content is test2
-
-    var pizzas = document.getElementById("vegan-pizzas");
-    if (pizzas.style.display === 'none') {
-        pizzas.style.display = 'block';
-    }
-    document.getElementById("all-pizzas").style.display = 'none';
-    document.getElementById("vegetarian-pizzas").style.display = 'none';
 }
