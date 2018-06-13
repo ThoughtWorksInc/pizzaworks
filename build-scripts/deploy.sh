3d4bc60730478ebdcc771be020940ff9db7e0500
Anthony Scatchell <scatchell@gmail.com>
2018-06-13 16:16:49 +0100

    31 [Kavita, Jen, Anthony] - Adding qa properties file

--- build-scripts/deploy.sh
+++ build-scripts/deploy.sh
@@ -11,5 +11,6 @@ scp  -r migrations  ec2-user@$PIZZA_WORKS_SERVER:/home/ec2-user/
 
 
 logger environment: $ENV
+logger pghost: $PGHOST
 echo "---- Starting Server on Remote box ----"
 ssh ec2-user@$PIZZA_WORKS_SERVER -o SendEnv="PGHOST PGPORT PGUSER PGDATABASE PGPW ENV" "chmod +x ./start-server.sh && ./start-server.sh"
